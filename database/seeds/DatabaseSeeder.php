69f26b142cde16d7293980f1d5095d027ed23790
Luis Rivero <riveromontes@gmail.com>
2017-07-20 12:50:05 -0430

    Modificaciones en Modelo Alumno y en Seed DatabaseSedder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        
+        //$this->call('AvionesSeeder');
+        //$this->call('AlumnosSeeder');
+        //$this->call('InstructoresSeeder');
+        //$this->call('VuelosSeeder');
+        //$this->call('DictanSeeder');
+        $this->call('PagosSeeder');
     }
 }
